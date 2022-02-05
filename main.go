14040a1a0040cdf3c2a1b354a75c20ae3e182e47
Widlund, Emil <ewidlund@ea.com>
2022-02-05 03:09:20 +0100

    Add noise generator

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"math"
+
+	"github.com/aquilax/go-perlin"
 	"github.com/emilwidlund/esmerelda/core"
 	"github.com/emilwidlund/esmerelda/vectors"
 )
 
 func main() {
-	formula := func(vector *vectors.Vector2) *vectors.Vector2 {
-		x := 1.
-		y := vector.Y*vector.Y - vector.Y
 
-		return vectors.NewVector2(x, y)
+	p := perlin.NewPerlin(2, 2, 3, 0)
+
+	formula := func(vector *vectors.Vector2) *vectors.Vector2 {
+		// x := 1.
+		// y := vector.Y*vector.Y - vector.Y
+
+		n := p.Noise2D(vector.X/20, vector.Y/20) * 10
+
+		return vectors.NewVector2(math.Cos(n), math.Sin(n))
 	}
 
 	field := core.NewVectorField(800, 600, formula)
